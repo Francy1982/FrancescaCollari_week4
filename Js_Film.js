b4a56aa6cb0e3bc89561509834584645e974518f
Francy1982 <101341831+Francy1982@users.noreply.github.com>
2022-03-25 15:33:01 +0100

    Add files via upload

---
+++ Js_Film.js
@@ -0,0 +1,25 @@
+﻿
+function welcomeMessage() {
+    alert("Benvenuto nella nostra newsletter!");
+}
+
+function Salva() {
+
+    //let key1 = "Nuovo iscritto"
+    //let value1 = document.getElementById("Name").value;
+
+    //let key2 = "Cognome"
+    //let value2 = document.getElementById("Surname").value;
+
+    let key3 = "email_NuovoIscritto"
+    let value3 = document.getElementById("Mail").value;
+
+ 
+   /* localStorage.setItem(key1,value1);*/
+    //localStorage.setItem(key2, value2);
+    localStorage.setItem(key3, value3);
+ 
+
+    
+   
+}
